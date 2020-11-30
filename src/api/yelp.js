c8fef4bd8c73bbfed548c6aad128d4e7b1e77138
jcheng <c-chenhao.cheng@charter.com>
2020-11-30 09:52:00 -0700

    remove token

--- src/api/yelp.js
+++ src/api/yelp.js
@@ -3,6 +3,6 @@ import axios from "axios";
 export default axios.create({
 	baseURL: "https://api.yelp.com/v3/businesses",
 	headers: {
-		Authorization: "Bearer JK_Mntat6pTmkjah6uV5lksUGuHCCNd_3oayQsBe_nZZyIhLHw0Gq94lbfiXwuLDcK6eO0XdgrS5JWXirb2VN7zBeVlTf6dvd07VLNVBclrWltW5vscP18B7DkbAX3Yx"
+		Authorization: "Bearer "
 	}
 });
